d4f7c4f837383f99c208769ad8330e8f6000ecdf
Anne O'Sullivan <osullivan_anne@hotmail.com>
2019-08-25 12:02:32 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
+import Navbar from './Navbar'
+import Content from './Content'
+import { connect } from 'react-redux'
+import {
+  loadWeb3,
+  loadAccount,
+  loadToken,
+  loadExchange
+} from '../store/interactions'
+import { contractsLoadedSelector } from '../store/selectors'
+
+class App extends Component {
+  componentWillMount() {
+    this.loadWeb3()
+    this.loadBlockchainData(this.props.dispatch)
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData(dispatch) {
+    const web3 = loadWeb3(dispatch)
+    await web3.eth.net.getNetworkType()
+    const networkId = await web3.eth.net.getId()
+    await loadAccount(web3, dispatch)
+    const token = await loadToken(web3, networkId, dispatch)
+    if(!token) {
+      window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+    const exchange = await loadExchange(web3, networkId, dispatch)
+    if(!exchange) {
+      window.alert('Exchange smart contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    contractsLoaded: contractsLoadedSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(App)
